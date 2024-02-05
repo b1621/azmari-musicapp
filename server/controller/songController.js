3ce96a8fe1557c6ffdf4bbecc5359dd6a8621edb
b0 <bewuket1621@gmail.com>
2024-02-05 11:24:33 +0300

    create song controller created

--- server/controller/songController.js
+++ server/controller/songController.js
@@ -3,10 +3,45 @@ const asyncHandler = require("express-async-handler");
 const Song = require("../model/songModel");
 
 exports.getAllSongs = asyncHandler(async (req, res) => {
-  const songs = await Song.find({});
-  res.status(200).json({
-    total: songs.length,
-    songs,
-  });
+  try {
+    const songs = await Song.find({});
+    res.status(200).json({
+      total: songs.length,
+      songs,
+    });
+  } catch (error) {
+    res.status(500);
+    console.log(error);
+    throw new Error(error);
+  }
 });
-exports.createSong = asyncHandler(async (req, res) => {});
+exports.createSong = asyncHandler(async (req, res) => {
+  try {
+    const { title, artist, album, genre } = req.body;
+    if (!title || !artist || !album || genre) {
+      res.status(400);
+      throw new Error("all field should filled!!!");
+    }
+
+    const song = await Song.create({
+      title,
+      album,
+      artist,
+      genre,
+    });
+
+    if (song) {
+      res.status(201).json({
+        message: "song is created",
+        song,
+      });
+    } else {
+      res.status(400);
+      throw new Error("invalid song");
+    }
+  } catch (error) {
+    res.status(500);
+    console.log(error);
+    throw new Error(error);
+  }
+});
