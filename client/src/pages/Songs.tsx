6bb7bfcde955aa719772f2cc5573949118866f4c
b0 <bewuket1621@gmail.com>
2024-02-06 21:14:25 +0300

    creating a song background image

--- client/src/pages/Songs.tsx
+++ client/src/pages/Songs.tsx
@@ -1,5 +1,26 @@
+import styled from "@emotion/styled";
+
+const ImageElement = styled.img`
+  width: 85vw;
+  height: 40vh;
+`;
+const HeaderBox = styled.img`
+  width: 85vw;
+  border: 1px solid brown;
+  height: 40vh;
+`;
+const Box = styled.div`
+  margin: 10px;
+`;
+
 const Songs = () => {
-  return <div>Songs</div>;
+  return (
+    <Box>
+      <HeaderBox>
+        {/* <ImageElement src="/austin-neill-hgO1wFPXl3I-unsplash.jpg" alt="" /> */}
+      </HeaderBox>
+    </Box>
+  );
 };
 
 export default Songs;
