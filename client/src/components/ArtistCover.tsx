b062c851890917bc90031d39ed53a54fa0ef5b74
b0 <bewuket1621@gmail.com>
2024-02-08 10:59:52 +0300

    artist cover component is completed

--- client/src/components/ArtistCover.tsx
+++ client/src/components/ArtistCover.tsx
@@ -7,24 +7,82 @@ interface ArtistCoverProps {
 }
 
 const Box = styled.div`
-  border: 1px solid green;
+  display: flex;
+  max-width: 200px;
+  flex-direction: column;
+  align-items: center;
+  font-weight: 400;
+  text-align: center;
+  padding: 0 20px;
 `;
 const ArtistImage = styled.img`
-  border: 1px solid green;
+  aspect-ratio: 1;
+  object-fit: auto;
+  object-position: center;
+  width: 100%;
+  align-self: stretch;
+  border-radius: 50%;
+`;
+const Div2 = styled.div`
+  color: #fff;
+  margin-top: 16px;
+  font: 22px Inter, sans-serif;
 `;
 
+const Div3 = styled.div`
+  color: rgba(255, 255, 255, 0.6);
+  margin-top: 12px;
+  white-space: nowrap;
+  font: 18px Inter, sans-serif;
+`;
 const ArtistCover = ({
   artistImage,
   artistName,
   totalSongs,
 }: ArtistCoverProps) => {
   return (
+    // <Box>
+    //   <ArtistImage src={artistImage} />
+    //   <h2>{artistName}</h2>
+    //   <p>{totalSongs} Songs</p>
+    // </Box>
     <Box>
-      <ArtistImage src={artistImage} />
-      <h2>{artistName}</h2>
-      <p>{totalSongs} Songs</p>
+      <ArtistImage loading="lazy" src={artistImage} />
+      <Div2>{artistName}</Div2>
+      <Div3>{totalSongs}</Div3>
     </Box>
   );
 };
 
 export default ArtistCover;
+
+// const Div = styled.div`
+//   display: flex;
+//   max-width: 200px;
+//   flex-direction: column;
+//   align-items: center;
+//   font-weight: 400;
+//   text-align: center;
+//   padding: 0 20px;
+// `;
+
+// const Img = styled.img`
+//   aspect-ratio: 1;
+//   object-fit: auto;
+//   object-position: center;
+//   width: 100%;
+//   align-self: stretch;
+// `;
+
+// const Div2 = styled.div`
+//   color: #fff;
+//   margin-top: 16px;
+//   font: 22px Inter, sans-serif;
+// `;
+
+// const Div3 = styled.div`
+//   color: rgba(255, 255, 255, 0.6);
+//   margin-top: 12px;
+//   white-space: nowrap;
+//   font: 18px Inter, sans-serif;
+// `;
--- client/src/pages/Artists.tsx
+++ client/src/pages/Artists.tsx
@@ -1,5 +1,15 @@
+import ArtistCover from "../components/ArtistCover";
+
 const Artists = () => {
-  return <div>Artists</div>;
+  return (
+    <div>
+      <ArtistCover
+        artistName="Ed Sheeran"
+        artistImage="https://imgs.search.brave.com/V4n370-nzAIon9wSaDLZqhgBKk1V0H5DCSTftYRC5Ks/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzAxL2Ew/Lzk1LzAxYTA5NTIy/ZjIzNGE3NmIzMTE5/MDc2NTMyZjc2YjQw/LmpwZw"
+        totalSongs={32}
+      />
+    </div>
+  );
 };
 
 export default Artists;
