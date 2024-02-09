dab34f5dd317cf51d3a98f12d0d90e71b317767b
b0 <bewuket1621@gmail.com>
2024-02-09 22:20:58 +0300

    configuring saga on toolkit but have warning

--- client/src/store.tsx
+++ client/src/store.tsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import songSlice from "./features/songSlice";
+import createSagaMiddleware from "redux-saga";
 
+const saga = createSagaMiddleware();
 export const store = configureStore({
   reducer: { song: songSlice },
+  middleware: [saga],
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
