382932c18e72592cb2a653c62692c10e2ae357a3
b0 <bewuket1621@gmail.com>
2024-02-13 16:15:59 +0300

    delete modal styled

--- client/src/components/DeleteModal.tsx
+++ client/src/components/DeleteModal.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import styled from "@emotion/styled";
 
-const DeleteModal = ({ setIsDeleteOpen }) => {
-  return <div>DeleteModal</div>;
+const BackgroundOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  opacity: 0.5;
+  z-index: 10;
+  background-color: #111111;
+`;
+
+const FormBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -70%);
+  background-color: #1d2339;
+  color: white;
+  border-radius: 10px;
+  width: 30%;
+  padding: 20px;
+  z-index: 11;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  margin-right: 20px;
+  cursor: pointer;
+  border: none;
+`;
+
+const DeleteButton = styled(Button)`
+  background-color: #dc381f;
+`;
+
+const DeleteModal = ({ setIsDeleteOpen, handleConfirm }) => {
+  return (
+    <>
+      <BackgroundOverlay />
+      <FormBox>
+        <h2>Confirm the action</h2>
+        <p>Are you sure you want to confirm?</p>
+        <div>
+          <Button onClick={() => setIsDeleteOpen(false)}>Cancel</Button>
+          <DeleteButton onClick={handleConfirm}>Delete</DeleteButton>
+        </div>
+      </FormBox>
+    </>
+  );
 };
 
 export default DeleteModal;
